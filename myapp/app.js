ec06d216c548237fde4984bafa28c5a0d4f399bb
Jad <zeineddine.jad@gmail.com>
2018-01-20 11:26:40 -0500

    endpoint fix

--- myapp/app.js
+++ myapp/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var learning = require('./learning/train');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
