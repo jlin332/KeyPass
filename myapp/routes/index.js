5ea2ab419985994f6100a1e7c472b6bf2bcf2d9a
Jad <zeineddine.jad@gmail.com>
2018-01-20 11:18:17 -0500

    added endpoint for train and login

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var json = require('jsonify');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,6 +9,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/john', function(req, res, next){
   res.render('LoginTemplate');
+})
+
+router.post('/train', function(req, res, next){
+  console.log(json.stringify(req.body()));
+})
+
+router.post('/login', function(req, res, next){
+
 })
 
 module.exports = router;
