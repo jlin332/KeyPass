a84a45250a61f71e4e54fd63cc146a2aaa3de736
John Lin <bigjohnlin@gmail.com>
2018-01-20 23:49:09 -0500

    In-between

--- myapp/learning/trainv2.js
+++ myapp/learning/trainv2.js
@@ -9,28 +9,34 @@ class train {
 
   addData(pressdown, inbetween){
     if(this.data_pressdown == undefined){
-      this.data_pressdown.push(toadd);
+        this.data_pressdown.push(pressdown);
     }
 
-    if(this.data_inbetween == undefined){
-      this.data_inbetween == undefined);
+    if(this.data_inbetween == undefined) {
+        this.data_inbetween.push(inbetween)
     }
 
-    else{
+    else {
       average_add_pressdown(pressdown);
       average_add_inbetween(inbetween);
     }
-
   }
 
   average_add_pressdown(pressdown){
     //averages into data_pressdown
-    
+
   }
 
-  average_add_inbetween(inbetween){
+  average_add_inbetween(inbetween) {
     //averages into data_inbetween
-
+    if (this.data_inbetween.length == 0) {
+        this.data_inbetween = inbetween;
+    } else {
+        for (i = 0; i < inbetween.length; i++ ) {
+            var avg = (inbetween[i] + this.data_inbetween[i])/2;
+            this.data_inbetween[i] = avg;
+        }
+    }
   }
 
   // {input: { r: 0.03, g: 0.7, b: 0.5 }, output: 0},  // black
@@ -40,9 +46,19 @@ class train {
     return callback();
   }
 
-  classify(data){
+  classify(press, between){
     console.log("Classifying...");
     //jacardi comparison
+    var between_index = jaccard.index(between, this.data_inbetween);
+    var new_arr = [];
+    var new_login = [];
+    for (a = 0; a < press.length; a++) {
+        new_arr[a] = press[a][1];
+        new_login = this.data_pressdown[a][1];
+    }
+    var pressdown_index = jaccard.index(new_arr, new_login);
+    // 75-25 ratio for jaccari index
+    return ((pressdown_index * 0.25) + (between_index * 0.8));
   }
 }
 
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var json = require('jsonify');
-const train = require('../learning/train');
+const train = require('../learning/trainv2');
 var router = express.Router();
 
 var trainer = new train();
@@ -11,28 +11,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/john', function(req, res, next){
     res.render('index');
-})
+});
 
 router.post('/train', function(req, res, next){
     var username = req.get("username");
     var password = req.get("password");
     var data = req.get("data");
-    //console.log(data);
-    //console.log(req.body["user"]);
+
     trainer.addData(data, req.body["user"]);
     res.status(200).send();
-})
+});
 
 router.post('/login', function(req, res, next){
-  //console.log("Verifying with...");
-  //console.log(req.get("data"));
   trainer.trainer(function () {
     var score = trainer.classify(req.get("data"));
     //console.log(score + " " + req.body["user"]);
     res.status(200).send(score);
-  })
-
-
-})
+    });
+});
 
 module.exports = router;
