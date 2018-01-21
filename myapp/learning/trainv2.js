5dad128b4152cdbf0cc05caea947216e211df300
Jad <zeineddine.jad@gmail.com>
2018-01-20 23:47:36 -0500

    added my v2 stuff

--- myapp/learning/trainv2.js
+++ myapp/learning/trainv2.js
@@ -9,23 +9,41 @@ class train {
 
   addData(pressdown, inbetween){
     if(this.data_pressdown == undefined){
-      this.data_pressdown.push(toadd);
+      this.data_pressdown.push(pressdown);
     }
-
     if(this.data_inbetween == undefined){
-      this.data_inbetween == undefined);
+      this.data_inbetween.push(inbetween);
     }
-
-    else{
+    else if(is_same(pressdown)){
       average_add_pressdown(pressdown);
       average_add_inbetween(inbetween);
     }
+    else{
 
+    }
+  }
+
+  is_same(pressdown){
+    if(this.data_pressdown.length != pressdown.length){
+      return false;
+    }
+    else{
+      for(var i in pressdown){
+        if(pressdown[i] != this.data_pressdown[i]){
+          return false;
+        }
+      }
+      return true;
+    }
   }
 
   average_add_pressdown(pressdown){
     //averages into data_pressdown
-    
+    for(var i in pressdown) {
+      var newval = (pressdown[i] + this.data_pressdown[i])/2;
+      this.data_pressdown[i] = newval;
+    }
+
   }
 
   average_add_inbetween(inbetween){
